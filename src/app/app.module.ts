207fd3a7c4e6e453384cd83e0e717353baa2f617
vademich <goncharov1999vadim@gmail.com>
2024-06-17 17:23:55 +0300

    provide Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './views/main/main.component';
 import { AboutComponent } from './views/about/about.component';
@@ -10,19 +11,21 @@ import { Bike3dComponent } from './views/bike3d/bike3d.component';
 import { ServicesComponent } from './views/services/services.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    AboutComponent,
-    BikesComponent,
-    Bike3dComponent,
-    ServicesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        MainComponent,
+        AboutComponent,
+        BikesComponent,
+        Bike3dComponent,
+        ServicesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        provideAnimationsAsync()
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,10 +1,28 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.less'
+    selector: 'app-main',
+    animations: [
+        trigger('text', [
+            state('off',
+                style({
+                    transform: 'translateX(0px)',
+                })),
+            state('on',
+                style({
+                    transform: 'translateX(70px)',
+                })),
+            transition('on => off', [animate('0.2s')]),
+            transition('off => on', [animate('0.2s')])
+        ])
+    ],
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.less'
 })
 export class MainComponent {
-
+    isHover = false;
+    textHover() {
+        this.isHover = !this.isHover;
+    }
 }
